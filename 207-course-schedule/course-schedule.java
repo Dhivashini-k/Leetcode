8f3bac2c01105db595e0fd1227204fab5f2de24a
Dhivashini <dhivashini608@gmail.com>
2026-06-17 09:19:05 +0530

    Time: 8 ms (25.79%) | Memory: 47.1 MB (47.46%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,36 @@
+class Solution {
+    public Boolean canFinish(int n, int[][] p) {
+        //create a adj list and indegree Array
+        int indegree[] = new int[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int [] edge : p){
+            int a = edge[0] , b = edge[1];
+            indegree[a]++;
+            adj.get(b).add(a);
+        }
+        //find the topological sort 
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        int [] ans = new int[n];
+        int i=0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            i++;
+            //check the adjacdnt connestions 
+            for(int a : adj.get(node)){
+                indegree[a]--;
+                if(indegree[a]==0){
+                    q.offer(a);
+                }
+            }
+        }
+        return i==n? true: false;
+    }
+}
